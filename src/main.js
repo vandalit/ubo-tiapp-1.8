295cacb95fd774f6c70e938f8b1a9560dadfae4a
Van <84937597+vandalit@users.noreply.github.com>
2025-01-30 18:40:10 -0300

    init

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
+// Importa el archivo SCSS personalizado
+import './assets/scss/main.scss';
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+// Importa el CSS de Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// (Opcional) Importa el JS de Bootstrap si necesitas funcionalidades interactivas
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createApp(App).use(store).use(router).mount('#app')
